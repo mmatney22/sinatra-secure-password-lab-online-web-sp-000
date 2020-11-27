3dfb857e824b1bcb695f7daa275f7eb3563487e0
Morgan Matney <morgan.matney89@gmail.com>
2020-11-27 21:32:32 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -36,7 +36,12 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/login" do
-    ##your code here
+    user = User.find_by(:username => params[:username])
+    if user 
+      redirect '/account' 
+    else 
+      redirect '/failure'
+    end
   end
 
   get "/failure" do
